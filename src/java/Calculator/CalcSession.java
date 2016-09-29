0f1b6fdddfe911eadea1589df53ca8eefb12bdb0
Ilyas <Ilyas@Ilyas-PC>
2016-09-29 21:46:13 +0300

    404 error page added

---
+++ src/java/Calculator/CalcSession.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculator;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ilyas
+ */
+public class CalcSession implements Serializable{
+    String sessionID;
+    ArrayList<String>alist;
+    
+    
+    
+}
